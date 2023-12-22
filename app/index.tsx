ac74685208fda4ecf6e29f3be790d76c3545d521
rofi <rfigueroa@truelogic.io>
2023-12-22 07:32:20 -0300

    x

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,22 @@
-import { Text, View } from "react-native";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 
 const Home = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <View className="flex-1 bg-gray-400 justify-end items-end p-8">
-      <View className="bg-black rounded-full w-[50] h-[50] items-center justify-center">
-        <Text className="text-white text-6xl">+</Text>
+    <View className="flex-1 bg-white justify-end items-end p-8 pb-28">
+      <Pressable className="" onPress={() => setToggle(!toggle)}>
+        <Text className="text-black text-6xl">Toggle</Text>
+      </Pressable>
+      <View
+        className={cn(
+          "duration-500 bg-black rounded-[100%] w-[50] h-[50] items-center justify-center",
+          "transition translate translate-y-0 transform",
+          toggle && "translate-y-20 rounded-[10%]"
+        )}
+      >
+        {/* <Text className="text-white text-6xl">+</Text> */}
       </View>
     </View>
   );
